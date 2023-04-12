e7abce0af420db5c1b3b98b3e824f0cf6ca82856
Keshav Pandey <88835211+RayLikesAnime@users.noreply.github.com>
2023-04-12 10:57:36 +0530

    last

--- src/hospital.php
+++ src/hospital.php
@@ -25,7 +25,7 @@ if(isset($_POST['logout'])){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
-    <title>Patient</title>
+    <title>Hospital</title>
 </head>
 <body>
 <header>
@@ -33,8 +33,6 @@ if(isset($_POST['logout'])){
             <div class="ml-4 flex items-center">
                 <img src="./images/logo.png" alt="">
                 <h1 class="text-3xl ml-2 font-semibold">LifeLink</h1>
-                <a href="Userpage.php"><button type="button" class=" ml-5 text-white bg-red-500 hover:bg-red-700 focus:ring-4 rounded-lg text-sm px-4 py-2 text-center inline-flex items-center">Back
-</button></a>
             </div>
             <ul class="flex justify-evenly mr-8">
                 <li class="text-lg font-semibold px-4"><a href="./Userpage.php">Home</a></li>
@@ -46,27 +44,14 @@ if(isset($_POST['logout'])){
             </ul>
         </nav>
     </header>
-    <div class="flex justify-center">
-    <a href="./registerhospital.php">
-<button class="bg-red-500  text-white mt-10 py-4 px-80 hover:bg-red-700  rounded border-red-500 ">
-  Register Hospital
+    <a href="./registerHospital.php">
+<button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+  Register A Hospital
 </button></a>
-</div>
-<div class="flex justify-center" >
-<a href="./searchhospital.php">
-<button class="bg-red-500 text-white py-4 px-80 mt-1 hover:bg-red-700 rounded border-red-500">
-  Search Hospital &nbsp;
+<a href="./searchHospital.php">
+<button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+  Search A Hospital
 </button>
 </a>
-</div>
-<div class="flex justify-center">
-    <a href="./deletehospital.php">
-<button class="bg-red-500 text-white mt-1 py-4 px-80 hover:bg-red-700 rounded border-red-500 ">
-  Delete Hospital &nbsp;&nbsp;
-</button></a>
-</div>
-<div class="w-screen mt-10 flex justify-center items-center">
-                <img src="./images/patient.jpg" alt="welcome" class="h-64" >
-            </div>
 </body>
 </html>
